b42f6971a0abd993acb8bbc5efc45e87a2d02ae7
aLMASIK <suleimenovalmas75@gmail.com>
2026-03-13 00:04:21 +0500

    Connect with another database

--- app/core/database.py
+++ app/core/database.py
@@ -7,11 +7,17 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_async_engine(DATABASE_URL, echo=False)
+engine = create_async_engine(
+    DATABASE_URL, 
+    echo=False,
+    pool_pre_ping=True, 
+    pool_recycle=300
+    )
 
 async_session = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 
 Base = declarative_base()
+
 
 
 async def get_db():
